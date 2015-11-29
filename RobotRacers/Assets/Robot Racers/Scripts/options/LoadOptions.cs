3594b7122f4afb37c89996b6299cb8dd1db17784
Daniel Martinez <dani3lmt@gmail.com>
2015-11-29 14:21:30 -0500

    Added methods to save user settings in options menu screen

---
+++ RobotRacers/Assets/Robot Racers/Scripts/options/LoadOptions.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
+
+public class LoadOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string lc = "A";
+		string rc = "D";
+		string uc = "W";
+		string dc = "S";
+		bool ss = true;
+		if (File.Exists (Application.persistentDataPath + "/playerSettings.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/playerSettings.dat", FileMode.Open);
+			PlayerOptions options = (PlayerOptions)bf.Deserialize(file);
+			file.Close();
+			lc = options.leftControl;
+			rc = options.rightControl;
+			uc = options.upControl;
+			dc = options.downControl;
+
+			ss = options.sound;
+		}
+
+		InputField leftControl = GameObject.Find ("configControlLeft").GetComponent<InputField> ();
+		InputField rightControl = GameObject.Find ("configControlRight").GetComponent<InputField> ();
+		InputField upControl = GameObject.Find ("configControlUP").GetComponent<InputField> ();
+		InputField downControl = GameObject.Find ("configControlDown").GetComponent<InputField> ();
+		Toggle sound = GameObject.Find ("ToggleSonidos").GetComponent<Toggle> ();
+
+		leftControl.text = lc;
+		rightControl.text = rc;
+		upControl.text = uc;
+		downControl.text = dc;
+
+		sound.isOn = ss;
+
+
+		/*leftControl.text = PlayerPrefs.GetString("controlLeft", "A");
+		rightControl.text = PlayerPrefs.GetString("controlRight", "D");
+		upControl.text = PlayerPrefs.GetString("controlUp", "W");
+		downControl.text = PlayerPrefs.GetString("controlDown", "S");*/
+	}
+}
+
+[Serializable]
+class PlayerOptions {
+	public string leftControl;
+	public string rightControl;
+	public string upControl;
+	public string downControl;
+
+	public bool sound;
+}
---
+++ RobotRacers/Assets/Robot Racers/Scripts/options/SaveOptions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveOptions : MonoBehaviour {
+	public void save() {
+		InputField leftControl = GameObject.Find ("configControlLeft").GetComponent<InputField> ();
+		InputField rightControl = GameObject.Find ("configControlRight").GetComponent<InputField> ();
+		InputField upControl = GameObject.Find ("configControlUP").GetComponent<InputField> ();
+		InputField downControl = GameObject.Find ("configControlDown").GetComponent<InputField> ();
+		Toggle sound = GameObject.Find ("ToggleSonidos").GetComponent<Toggle> ();
+
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/playerSettings.dat");
+		PlayerOptions options = new PlayerOptions ();
+
+		// Controles.
+		options.leftControl = leftControl.text != "" ? leftControl.text : "A";
+		options.rightControl = rightControl.text != "" ? rightControl.text : "D";
+		options.upControl = upControl.text != "" ? upControl.text : "W";
+		options.downControl = downControl.text != "" ? downControl.text : "S";
+
+		// Sonido.
+		options.sound = sound.isOn;
+
+		bf.Serialize (file, options);
+		file.Close ();
+		print ("Se han guardado la opciones");
+	}
+}
