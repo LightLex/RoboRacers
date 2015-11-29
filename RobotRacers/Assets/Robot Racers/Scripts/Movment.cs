e4822fc392b2a465cda0621ff1b032f8efbffc34
LightLex <lexxxiss@gmail.com>
2015-11-28 20:48:51 -0500

    Adding models and map

---
+++ RobotRacers/Assets/Robot Racers/Scripts/Movment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movment : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+
+	void Update(){
+		if(Input.GetKey(KeyCode.W)){
+			speed=20;
+			transform.Translate(new Vector3(0,0,speed)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			speed=-20;
+			transform.Translate(new Vector3(0,0,speed)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			speed=20;
+			transform.Translate(new Vector3(speed,0,0)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			speed=-20;
+			transform.Translate(new Vector3(speed,0,0)*Time.deltaTime);
+		}
+
+	}
+}
