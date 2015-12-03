76c0e70d227fcb65996f3edf8762f14072de11e6
LightLex <lexxxiss@gmail.com>
2015-12-03 18:45:51 -0500

    Modding 3

---
+++ RobotRacers/Assets/Robot Racers/Scripts/TargetFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetFollow : MonoBehaviour {
+	public Transform target;
+	public float distUP;
+	public float distBack;
+	public float minHeight;
+
+	private Vector3 positionVel;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 newPosition = target.position + (target.forward * distBack);
+		newPosition.y = Mathf.Max (newPosition.y + distUP, minHeight);
+
+		transform.position = Vector3.SmoothDamp (transform.position, newPosition, ref positionVel, 0.18f);
+
+		Vector3 point = target.position + (target.forward * 5);
+
+		transform.LookAt (point);
+	}
+}
